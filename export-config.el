6a32944f9bc633447fa93af9a30836b2dc9ed8f7
ethanxxxl <ethansmith.dev@gmail.com>
2023-05-14 22:38:11 -0400

    Updated structure to work with publish function

--- export-config.el
+++ export-config.el
@@ -2,20 +2,21 @@
 ;; this config is heavily influenced by DistroTube's configuration
 (setq org-publish-local-alist
       '(("static"
-          :base-directory "./"
+          :base-directory "./resources"
           :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
           :publishing-directory "./html"
           :recursive t
           :publishing-function org-publish-attachment)
 
          ("org-files"
-          :base-directory "./"
+          :base-directory "./org/"
           :base-extension "org"
           :publishing-directory "./html"
           :recursive t
           :publishing-function org-html-publish-to-html
           :headline-levels 4            ; Just the default for this project.
-          :auto-preamble t)
+          :auto-preamble t
+          :with-properties nil)
 
          ("personal_webpage"
           :components ("org-files" "static"))))
