2673c92695fc4f792395bd0c09c459bb41297f3c
ethanxxxl <ethansmith.dev@gmail.com>
2023-06-04 04:42:49 -0500

    appended '/' to search directories. added html5
    I changed the publishing script to fix the weird problems I was having
    with the include files. As a result of those fixes, it is now a
    requirement to properly format the directories in this file.

--- export-config.el
+++ export-config.el
@@ -2,22 +2,23 @@
 ;; this config is heavily influenced by DistroTube's configuration
 (setq org-publish-local-alist
       '(("static"
-          :base-directory "./resources"
-          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-          :publishing-directory "./html"
-          :recursive t
-          :publishing-function org-publish-attachment)
+         :base-directory "./resources/"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :publishing-directory "./html/"
+         :recursive t
+         :publishing-function org-publish-attachment)
 
-         ("org-files"
-          :base-directory "./org/"
-          :base-extension "org"
-          :publishing-directory "./html"
-          :recursive t
-          :publishing-function org-html-publish-to-html
-          :headline-levels 4            ; Just the default for this project.
-          :html-postamble nil
-          :with-properties nil
-          :with-tags nil)
+        ("org-files"
+         :html-doctype "html5"
+         :base-directory "./org/"
+         :base-extension "org"
+         :publishing-directory "./html/"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4             ; Just the default for this project.
+         :html-postamble nil
+         :with-properties nil
+         :with-tags nil)
 
          ("personal_webpage"
           :components ("org-files" "static"))))
